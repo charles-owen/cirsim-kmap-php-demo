30086d5092ecf82df3a8c8f8890c63980b64f495
cbowen <cbowen@cse.msu.edu>
2021-01-17 21:03:53 -0500

    Added documentation, some cleanup

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'lib/demo.inc.php';
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1" name="viewport">
     <?php require 'lib/cirsim.inc.php'; ?>
-    <title>Cirsim Demonstration</title>
+    <title>Cirsim and Kmap Demonstrations</title>
 </head>
 <body>
 
@@ -18,7 +18,7 @@ require 'lib/demo.inc.php';
     <li><a href="testing.php">Testing demo</a></li>
     <li><a href="import.php">Cirsim Tab Import</a></li>
     <li><a href="kmap.php">Karnaugh Map Editor</a></li>
-    <li><a href="kmap-test.php">Karnaugh Map Test Result</a></li>
+    <li><a href="kmap-test.php">Karnaugh Map Test</a></li>
 </ul>
 <ul>
     <li><a href="tests.php">Test results</a></li>
@@ -26,7 +26,6 @@ require 'lib/demo.inc.php';
 
 <h2>Cirsim Inline</h2>
 <?php
-$adapter = new Demo\CirsimDemo();
 $cirsim = new CL\CirsimPHP\Cirsim();
 $json = '{"grid":8,"snap":true,"circuits":[{"name":"main","width":350,"height":80,"components":[{"id":"c1001","x":48,"y":24,"name":"A","type":"InPin","value":false},{"id":"c1002","x":48,"y":56,"name":"B","type":"InPin","value":false},{"id":"c1003","x":296,"y":40,"name":"L1","type":"LED","color":"blue"},{"id":"c1004","x":176,"y":40,"name":null,"type":"Or"}],"connections":[{"from":"c1001","out":0,"to":"c1004","in":0,"bends":[]},{"from":"c1002","out":0,"to":"c1004","in":1,"bends":[]},{"from":"c1004","out":0,"to":"c1003","in":0,"bends":[]}]}]}';
 echo $cirsim->present_demo($json);
